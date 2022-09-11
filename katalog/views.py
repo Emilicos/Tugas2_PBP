9593030835f3fa87859a510c982093e5c5fe3234
emilicos <notalvaroaustin@gmail.com>
2022-09-12 00:46:04 +0700

    finished making server

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# TODO: Create your views here.
+from katalog.models import CatalogItem
+
+def show_katalog(request):
+    katalog = CatalogItem.objects.all()
+    context = {
+        "nama": "Alvaro Austin",
+        "student_id": 2106752180,
+        "katalog": katalog
+    }
+    return render(request, 'katalog.html', context)
